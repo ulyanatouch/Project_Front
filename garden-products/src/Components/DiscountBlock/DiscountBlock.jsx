0de4eac1a841b7378320298dbcd38e57c72f796b
Uliana B. <ulyana9963@gmail.com>
2024-01-05 20:35:21 +0100

    add new components

--- garden-products/src/Components/DiscountBlock/DiscountBlock.jsx
+++ garden-products/src/Components/DiscountBlock/DiscountBlock.jsx
@@ -5,12 +5,14 @@ import { FormComponent } from "../FormComponent/FormComponent";
 
 const DiscountBlock = () => {
   return (
-    <div className="discount-block">
-      <h1 className="discount-block__title">5% off on the first order</h1>
+    <div className="discount-container">
+      <div className="discount-block">
+        <h1 className="discount-block__title">5% off on the first order</h1>
 
-      <div className="discount-block__content">
-        <img src={hands} alt="hands" />
-        <FormComponent />
+        <div className="discount-block__content">
+          <img src={hands} alt="hands" />
+          <FormComponent />
+        </div>
       </div>
     </div>
   );
--- garden-products/src/Components/Input/Input.jsx
+++ garden-products/src/Components/Input/Input.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Input.scss";
 
-const Input = ({ type, value, onChange, placeholder }) => {
+const Input = ({type, value, onChange, placeholder}) => {
   return (
-    <input className="custom-input"
-      type={type}
-      value={value}
-      onChange={(event) => onChange(event.target.value)}
-      placeholder={placeholder}
-    />
+      <div className="custom-input-container">
+          <input
+              className="custom-input"
+              type={type}
+              value={value}
+              onChange={(event) => onChange(event.target.value)}
+              placeholder={placeholder}
+          />
+      </div>
   );
 };
-export { Input };
+export {Input};

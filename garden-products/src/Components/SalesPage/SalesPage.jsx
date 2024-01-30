cfad567a0db9b5d3a7a78f50b81bb918524113a3
Uliana B. <ulyana9963@gmail.com>
2024-01-30 21:58:40 +0100

    add new components

--- garden-products/src/Components/SalesPage/SalesPage.jsx
+++ garden-products/src/Components/SalesPage/SalesPage.jsx
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./SalesPage.scss";
 import { PagesContainer } from "../PagesContainer/PagesContainer";
 import { fetchProducts } from "../../requests";
-import { Breadcrumbs } from "../BreadCrumbs/BreadCrumbs";
 
 const SalesPage = () => {
   const [cards, setProducts] = useState([]);
-    const productsWithDiscount = cards.filter((product) => product.discont_price)
+  const productsWithDiscount = cards.filter((product) => product.discont_price);
 
   useEffect(() => {
     fetchProducts(setProducts);
   }, []);
 
   return (
-  <div>
-    <Breadcrumbs/>
-  <PagesContainer title="All sales" cardsData={productsWithDiscount} />;
-  </div>
-  )
+    <div>
+      <PagesContainer title="All sales" cardsData={productsWithDiscount} />;
+    </div>
+  );
 };
 
 export { SalesPage };
